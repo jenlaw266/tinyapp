7d674185b5610e40c16e523911686b4e93d87a85
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-30 21:39:28 -0700

    added url_new.ejs; installed body-parser; added a random 6char generator

--- express_server.js
+++ express_server.js
@@ -2,7 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  const array = [];
+  const random = (x) => {
+    return Math.floor(Math.random() * x);
+  };
+  for (let i = 0; i < 6; i++) {
+    if (random(2)) {
+      array.push(random(10));
+    } else {
+      if (random(2)) {
+        array.push(String.fromCharCode(random(26) + 97));
+      } else {
+        array.push(String.fromCharCode(random(26) + 65));
+      }
+    }
+  }
+  console.log(array.join(""));
+  return array.join("");
+}
+generateRandomString();
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -11,6 +39,10 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
