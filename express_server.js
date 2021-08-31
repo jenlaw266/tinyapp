7dc391b2242b1d92daca21f626c848a253b622c8
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-31 13:12:29 -0700

    redirect shortURL to longURL and installed nnodemon

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8090; // default port 8080
 
 function generateRandomString() {
   const array = [];
@@ -18,24 +18,25 @@ function generateRandomString() {
       }
     }
   }
-  console.log(array.join(""));
   return array.join("");
 }
-generateRandomString();
+
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.post("/urls", (req, res) => {
+  const key = generateRandomString();
+  urlDatabase[key] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${key}`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -55,6 +56,11 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
