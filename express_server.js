cfc49c90cdc669f2ff9e504bc727cf8e3b5e1653
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-09-01 16:44:56 -0700

    added register page, changed login endpoint accordingly, added register errors

--- express_server.js
+++ express_server.js
@@ -24,41 +24,115 @@ function generateRandomString() {
   return array.join("");
 }
 
+function emailLookUp(inputEmail) {
+  for (const key in users) {
+    if (users[key].email === inputEmail) {
+      return key;
+    }
+  }
+  return false; //no email match
+}
+
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 //main page
 app.get("/urls", (req, res) => {
+  const userID = req.cookies["user_id"];
+
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[userID],
     urls: urlDatabase,
   };
+
   res.render("urls_index", templateVars);
 });
 
 //login
 app.post("/urls", (req, res) => {
-  const username = req.body.login;
-  res.cookie("username", username);
+  const inputEmail = req.body.login;
 
-  res.redirect("/urls");
+  if (emailLookUp(inputEmail)) {
+    res.cookie("user_id", emailLookUp(inputEmail));
+    res.redirect("/urls");
+  }
+  res.redirect("/register");
 });
 
 //logout
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
 
+//register page
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+    error: null,
+  };
+  res.render("urls_register", templateVars);
+});
+
+//register page - form
+app.post("/register", (req, res) => {
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+
+  //error message - if one of the register input field is empty
+  if (!userEmail || !userPassword) {
+    const templateVars = {
+      user: users[req.cookies["user_id"]],
+      error: "one of the field is empty",
+    };
+    res.status(400).render("urls_register", templateVars);
+  }
+
+  //error message - if email has already been registered
+  if (emailLookUp(userEmail)) {
+    const templateVars = {
+      user: users[req.cookies["user_id"]],
+      error: "this email has already been registered",
+    };
+    res.status(400).render("urls_register", templateVars);
+  }
+
+  //error message - already logged in
+  if (req.cookies["user_id"]) {
+    const templateVars = {
+      user: users[req.cookies["user_id"]],
+      error: "You are currently logged in",
+    };
+    res.status(400).render("urls_register", templateVars);
+  }
+
+  const id = generateRandomString();
+  users[id] = {
+    id: id,
+    email: userEmail,
+    password: userPassword,
+  };
+  res.cookie("user_id", id);
   res.redirect("/urls");
 });
 
@@ -71,14 +145,14 @@ app.post("/urls/new", (req, res) => {
 
 //new url
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
 //shortURL page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
